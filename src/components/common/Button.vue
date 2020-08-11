d69183f62bf6bf556bddfa570f077f0e4c11e7cf
lukas.sin <lukas.sin@kakaocorp.com>
2020-08-11 21:16:04 +0900

    ADD : Input, Button 컴포넌트 ;

---
+++ src/components/common/Button.vue
@@ -0,0 +1,12 @@
+<template>
+  <button v-on="$listeners">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/common/Input.vue
@@ -0,0 +1,39 @@
+<template>
+  <input type="text" @input="input" :value="computedValue" @focus="focus" @blur="blur" ref="input" />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String
+    }
+  },
+  computed: {
+    computedValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      }
+    }
+  },
+  methods: {
+    input(e) {
+      this.computedValue = e.target.value;
+    },
+    focus() {
+      //   this.$refs.input.focus();
+      this.$emit("focus");
+    },
+    blur() {
+      //   this.$refs.input.blur();
+      this.$emit("blur");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
